d292c986e07a3d2d65ccc36815ed19e123028c7c
scottctaylor12 <>
2018-02-24 22:14:18 -0500

    Add support for asciidoc

---
+++ _plugins/asciidoc.rb
@@ -0,0 +1,14 @@
+require 'asciidoc/macrohelper'
+
+require 'asciidoc/asciiplugins'
+require 'asciidoc/asciicharplugins'
+
+require 'asciidoc/asciielement'
+require 'asciidoc/asciilines'
+require 'asciidoc/asciiblock'
+require 'asciidoc/asciichars'
+
+Dir[File.dirname(__FILE__) + '/asciidoc/plugins/lines/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/asciidoc/plugins/chars/*.rb'].each {|file| require file }
+
+require 'asciidoc/asciidocument'
